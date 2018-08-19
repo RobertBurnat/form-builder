d717ade109578a4b5fffcb3f06020d45385acf7b
Robert Burnat <burnat.robertt@gmail.com>
2018-08-19 13:00:06 +0200

    Move addSubInput to parent

--- src/components/InputsList/Input/Input.js
+++ src/components/InputsList/Input/Input.js
@@ -6,51 +6,21 @@ import './Input.css'
 
 class Input extends Component {
     state = {
-        subInput: [],
-        type: ''
+        type: '',
 }
-    
-    addSubInput = () => {
-        const conditionInput = {
-            id: uuid.v4(),
-            questionLabel: 'Question',
-            typeLabel: 'Type',
-            conditionLabel: 'Condition',
-            types: [
-              {value: '', name: ''},
-              {value: 'Text', name: 'Text'},
-              {value: 'Number', name: 'Number'},
-              {value: 'YesNo ', name: 'YesNo'}
-            ],
-            conditions: [
-              { value : 'Equals', name: 'Equals'},
-              { value: 'Greater than', name: 'Greater than'},
-              { value: 'Less than', name: 'Less than'}
-            ],
-            radio: [
-              { value: 'Yes', name: 'Yes'},
-              { value: 'No', name: 'No'}
-            ],
-        };
-        const subInput = [...this.state.subInput, conditionInput];
-         this.setState({subInput});
-    }
-
     onChangeType = e => {
         const newState = {
             ...this.state.type
         }
         this.setState({type: e.target.value})
     }
-
     removeHandler = index => {
         const subInput = [...this.state.subInput]
         subInput.splice(index, 1);
         this.setState({subInput: subInput});
     }
-
     render() {
-        const test = this.state.subInput.map((cur, index) =>{
+        const test = this.props.subInput.map((cur, index) =>{
             return <SubInput
                 key={index}
                 id={cur.id}
@@ -61,7 +31,7 @@ class Input extends Component {
                 conditions={cur.conditions}
                 radio={cur.radio}
                 typeInput={this.state.type}
-                addSubIn={this.addSubInput}
+                addSubInput={this.props.addSubInput}
                 remove={this.removeHandler}
                 />
         })
@@ -70,7 +40,7 @@ class Input extends Component {
                 <div className="normInput">
                     <div className="contentInput">
                         <label className="question">{this.props.questionLabel}
-                        <input type="text" className="inputQuest" onChange={this.props.changed} /></label>
+                        <input type="text" className="inputQuest"/></label>
                         <label className="type">{this.props.typeLabel}
                         <select name="type" className="inputType" onChange={this.onChangeType}>
                             {this.props.types.map((currency, key) => {
@@ -78,7 +48,7 @@ class Input extends Component {
                         })}
                         </select></label>
                             <div className="Buttons">
-                                <button className="add" onClick={this.addSubInput}>Add Sub-Input</button>
+                                <button className="add" onClick={this.props.addSubInput}>Add Sub-Input</button>
                                 <button className="delete" onClick={e => this.props.remove(this.props.id)}>Delete</button>
                             </div>
                         </div>
