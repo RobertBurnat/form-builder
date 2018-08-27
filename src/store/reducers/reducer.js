13810cdf3123495d269ef07769178bb4becaef66
Robert Burnat <burnat.robertt@gmail.com>
2018-08-27 19:39:49 +0200

    init reducer

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,73 @@
+import * as actionTypes from '../actions';
+import uuid from 'uuid';
+
+const initialState = {
+    input: [],
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.ADD_INPUT:
+            const normalInput = {
+                id: uuid.v4(),
+                questionLabel: 'Question',
+                typeLabel: 'Type',
+                conditionLabel: 'Condition',
+                types: [
+                    {value: '', name: ''},
+                    {value: 'Text', name: 'Text'},
+                    {value: 'Number', name: 'Number'},
+                    {value: 'Yes / No', name: 'Yes / No'}
+                ],
+                subInput: []
+            }
+            return {
+                ...state,
+                input: [...state.input, normalInput]
+            }
+
+        case actionTypes.REMOVE_INPUT:
+            const remainder = state.input.filter((cur, index) => cur.id !== action.id);
+            return {
+                ...state,
+                input: remainder
+            }
+
+        case actionTypes.ADD_SUBINPUT:
+            const conditionInput = {
+                id: action.id,
+                questionLabel: 'Question',
+                typeLabel: 'Type',
+                conditionLabel: 'Condition',
+                types: [
+                  {value: '', name: ''},
+                  {value: 'Text', name: 'Text'},
+                  {value: 'Number', name: 'Number'},
+                  {value: 'Yes / No ', name: 'Yes / No'}
+                ],
+                conditions: [
+                  { value : 'Equals', name: 'Equals'},
+                  { value: 'Greater than', name: 'Greater than'},
+                  { value: 'Less than', name: 'Less than'}
+                ],
+                radio: [
+                  { value: 'Yes', name: 'Yes'},
+                  { value: 'No', name: 'No'}
+                ],
+            }
+            const input = state.input.map(cur => {
+                if(cur.id === action.id) {
+                    cur.subInput = [...cur.subInput, conditionInput];
+                }
+                return cur;
+            });
+            return {
+                ...state,
+                input: input
+            }
+
+    }
+    return state;
+}
+
+export default reducer;
